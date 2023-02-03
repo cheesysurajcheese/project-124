564732cb33887ed816dad71621b8866666a7e1f4
cheesysurajcheese <87599069+cheesysurajcheese@users.noreply.github.com>
2023-02-03 17:49:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -8,6 +12,10 @@ function setup(){
 }
 function draw(){
     background("#969A97");
+    document.getElementById("font_size").innerHTML = "Font-size of the text will be = " + difference + "px";
+    textSize(difference);
+    fill("#F90093");
+    text("Suraj",50, 400);
 
 }
 function modelLoaded(){
@@ -16,6 +24,11 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
+
         
     }
 }
